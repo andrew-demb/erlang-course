8016749e38d93615c221f5f042f25b2f76645212
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-02 00:38:02 +0200

    Lesson2: P13 add todo (wrong nested lists)

--- lesson_2/p13.erl
+++ lesson_2/p13.erl
@@ -5,6 +5,8 @@
 % p13:decode([{4,a},{1,b},{2,c},{2,a},{1,d},{4,e}]).
 % [a,a,a,a,b,c,c,a,a,d,e,e,e,e]
 
+% ToDo: how to avoid extra nesting?
+
 decode([{C,Symbol}|T]) ->
     [decode_symbol(C, Symbol, []) | decode(T)];
 
