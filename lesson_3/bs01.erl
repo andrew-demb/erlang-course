f77156b4e7243936cdfc287ae30c3a05c74bf672
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-02 20:32:30 +0200

    Lesson3: BS01 Finished (handle no space)

--- lesson_3/bs01.erl
+++ lesson_3/bs01.erl
@@ -12,4 +12,7 @@ first_word(<<" ", _Rest/binary>>, Acc) ->
     Acc;
 
 first_word(<<X/utf8, Rest/binary>>, Acc) ->
-    first_word(Rest, <<Acc/binary,X/utf8>>).
+    first_word(Rest, <<Acc/binary,X/utf8>>);
+
+first_word(<<"">>, Acc) ->
+    Acc.
