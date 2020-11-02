6164eab6db7ae4f1df53b84e703b1908da9a2839
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-02 18:56:15 +0200

    Lesson2: P07 finished (copied from lesson talk)

--- lesson_2/p07.erl
+++ lesson_2/p07.erl
@@ -5,6 +5,21 @@
 % p07:flatten([a,[],[b,[c,d],e]]).
 % [a,b,c,d,e]
 
-flatten(_) ->
-    % ToDo: find solution
-    [a,b,c,d,e].
+flatten(L) ->
+    L2 = flatten(L, []),
+    p05:reverse(L2).
+
+flatten([[]|T], Acc) ->
+    flatten(T, Acc);
+
+flatten([[_|_]=H|T], Acc) ->
+    flatten(T, flatten(H, Acc));
+
+flatten([H|T], Acc) ->
+    flatten(T, [H|Acc]);
+
+flatten([], Acc) ->
+    Acc;
+
+flatten(H, Acc) ->
+    [H | Acc].
