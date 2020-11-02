d4ec318fc8d2a0d91675d5eb9c62cbfd621644ad
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-02 20:26:37 +0200

    Lesson3: BS01 finished

---
+++ lesson_3/bs01.erl
@@ -0,0 +1,15 @@
+% BinText = <<"Some text">>.
+% <<"Some Text">>
+% bs01:first_word(BinText).
+
+-module(bs01).
+-export([first_word/1]).
+
+first_word(X) ->
+    first_word(X, <<>>).
+
+first_word(<<" ", _Rest/binary>>, Acc) ->
+    Acc;
+
+first_word(<<X/utf8, Rest/binary>>, Acc) ->
+    first_word(Rest, <<Acc/binary,X/utf8>>).
