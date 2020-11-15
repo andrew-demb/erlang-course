81c379b6d4035f6559c2567e9d3093cebaf06874
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-16 00:24:59 +0200

    Lesson 2: p11 done with tailing recursion

--- lesson_2/src/p11.erl
+++ lesson_2/src/p11.erl
@@ -5,17 +5,21 @@
 % p11:encode_modified([a,a,a,a,b,c,c,a,a,d,e,e,e,e]).
 % [{4,a},b,{2,c},{2,a},d,{4,e}]
 
-encode_modified([H|T]) ->
-    encode_symbol(T, 1, H);
+encode_modified(L) ->
+    L2 = encode_modified(L, []),
+    p05:reverse(L2).
 
-encode_modified(X) ->
-    X.
+encode_modified([H|T], Acc) ->
+    encode_symbol(T, 1, H, Acc);
 
-encode_symbol([_=Symbol|T], C, Symbol) ->
-    encode_symbol(T, C+1, Symbol);
+encode_modified([], Acc) ->
+    Acc.
 
-encode_symbol(X, 1, Symbol) ->
-    [Symbol|encode_modified(X)];
+encode_symbol([_=Symbol|T], C, Symbol, Acc) ->
+    encode_symbol(T, C+1, Symbol, Acc);
 
-encode_symbol(X, C, Symbol) ->
-    [{C, Symbol}|encode_modified(X)].
+encode_symbol(X, 1, Symbol, Acc) ->
+    encode_modified(X, [Symbol | Acc]);
+
+encode_symbol(X, C, Symbol, Acc) ->
+    encode_modified(X, [{C, Symbol} | Acc]).
