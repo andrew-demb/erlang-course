3117cd40335e7dc1ad2604f848ff2375d363891b
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-15 23:55:18 +0200

    Lesson 2: p09 done with tailing recursion

--- lesson_2/src/p09.erl
+++ lesson_2/src/p09.erl
@@ -6,16 +6,17 @@
 % [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
 
 pack([H|T]) ->
-    pack(T, [H]);
+    L2 = pack(T, [H], []),
+    p05:reverse(L2);
 
-pack(X) ->
-    X.
+pack([]) ->
+    [].
 
-pack([H=H1|T], [H1|Tail]) ->
-    pack(T, [H, H1|Tail]);
+pack([H=H1|T], [H1|Tail], Acc) ->
+    pack(T, [H, H1|Tail], Acc);
 
-pack([H|T], Tail) ->
-    [Tail | pack(T, [H])];
+pack([H|T], Tail, Acc) ->
+    pack(T, [H], [Tail | Acc]);
 
-pack([], Tail) ->
-    [Tail].
+pack([], Tail, Acc) ->
+    [Tail | Acc].
