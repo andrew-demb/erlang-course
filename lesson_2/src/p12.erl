3ad6c5641065cd5cc47ed24f9e4c8ea77cf2e947
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-22 16:05:20 +0200

    Lesson 2: p12 done

--- lesson_2/src/p12.erl
+++ lesson_2/src/p12.erl
@@ -9,17 +9,14 @@ decode_modified(L) ->
     L2 = decode_modified(L, []),
     p05:reverse(L2).
 
+decode_modified([{0,_}|T], Acc) ->
+    decode_modified(T, Acc);
+
 decode_modified([{C,Symbol}|T], Acc) ->
-    decode_modified(T, [decode_modified_symbol(C, Symbol, []) | Acc]);
+    decode_modified([{C-1,Symbol} | T], [Symbol | Acc]);
 
 decode_modified([Symbol|T], Acc) ->
-    decode_modified(T, [decode_modified_symbol(1, Symbol, []) | Acc]);
+    decode_modified(T, [Symbol | Acc]);
 
 decode_modified([], Acc) ->
     Acc.
-
-decode_modified_symbol(0, _, Tail) ->
-    Tail;
-
-decode_modified_symbol(C, Symbol, Tail) ->
-    decode_modified_symbol(C-1, Symbol, [Symbol|Tail]).
