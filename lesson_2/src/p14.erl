c52995cc866678e2f31ef4badde0de48ede1a33a
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-22 16:14:38 +0200

    Lesson 2: p14 done with tailing recursion

--- lesson_2/src/p14.erl
+++ lesson_2/src/p14.erl
@@ -5,11 +5,12 @@
 % p14:duplicate([a,b,c,c,d]).
 % [a,a,b,b,c,c,c,c,d,d]
 
-duplicate([H|T]) ->
-    duplicate_symbol(H, T);
+duplicate(L) ->
+    L2 = duplicate(L, []),
+    p05:reverse(L2).
 
-duplicate([]) ->
-    [].
+duplicate([H|T], Acc) ->
+    duplicate(T, [H, H | Acc]);
 
-duplicate_symbol(X, Y) ->
-    [X,X|duplicate(Y)].
+duplicate([], Acc) ->
+    Acc.
