2addef35b5c188fc3ddc09810d0b38969d79e82d
Andrii Dembitskyi <andrew.dembitskiy@gmail.com>
2020-11-28 22:35:54 +0200

    Lesson 2: fixed comment with expected result in demo

--- lesson_2/src/demo.erl
+++ lesson_2/src/demo.erl
@@ -1,12 +1,13 @@
 -module(demo).
 -export([double_num/1]).
 
+% test
+% c(demo).
+% demo:double_num(5).
+% 10
+
 double_num(X) ->
   times(X, 2).
 
 times(X, N) ->
   X * N.
-
-% c(demo).
-% demo:double_num(5).
-% 25
